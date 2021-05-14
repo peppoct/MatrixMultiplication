6e34a0032808b66ca7783569bb032e930a3475f3
armandomacri <armandomacri.96@gmail.com>
2021-05-14 16:43:56 +0200

    fix some issue (not working yet)

--- src/main/java/it/unipi/hadoop/matrix/MatrixMultiplication.java
+++ src/main/java/it/unipi/hadoop/matrix/MatrixMultiplication.java
@@ -1,9 +1,10 @@
+package it.unipi.hadoop.matrix;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
-
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -12,29 +13,26 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
-import org.apache.hadoop.yarn.webapp.hamlet2.Hamlet;
-
-import java.awt.dnd.DragGestureRecognizer;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class MatrixMultiplication {
 
     public static class MatrixMapper extends Mapper<LongWritable, Text, Text, Text>{
 
-        private Text outputKey = new Text();
-        private Text outputValue = new Text();
+        private final Text outputKey = new Text();
+        private final Text outputValue = new Text();
 
         @Override
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
             Configuration conf = context.getConfiguration();
 
-            int col_N = Integer.parseInt(conf.get("columns_N"));
-            int row_M = Integer.parseInt(conf.get(("rows_M")));
+            final int col_N = Integer.parseInt(conf.get("columns_N"));
+            final int row_M = Integer.parseInt(conf.get("rows_M"));
 
-
+            // (M, i, j, Mij)
             String[] values = value.toString().split(",");
             if (values[0].equals("M")){
                 for (int i=0; i<col_N; i++){
@@ -42,13 +40,12 @@ public class MatrixMultiplication {
                     outputValue.set(values[0]+","+values[2]+","+values[3]);
                     context.write(outputKey, outputValue);
                 }
-            } else if (values[0].equals("N")){
+            } else
                 for (int j=0; j<row_M; j++){
-                    outputKey.set(values[1]+","+j);
+                    outputKey.set(j + "," + values[2]);
                     outputValue.set(values[0]+","+values[1]+","+values[3]);
                     context.write(outputKey, outputValue);
                 }
-            }
         }
     }
 
@@ -57,8 +54,8 @@ public class MatrixMultiplication {
         @Override
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
 
-            HashMap<Integer, Float> M = new HashMap<>();
-            HashMap<Integer, Float> N = new HashMap<>();
+            Map<Integer, Float> M = new HashMap<>();
+            Map<Integer, Float> N = new HashMap<>();
 
             String[] riga;
             for (Text value : values){
@@ -72,21 +69,17 @@ public class MatrixMultiplication {
 
             Configuration conf = context.getConfiguration();
             int col_M = Integer.parseInt(conf.get("columns_M"));
-            float sum = 0.0f;
-            float m, n;
 
-            for (int j=0; j<col_M; j++){
-                if (M.containsKey(j) && N.containsKey(j)){
-                    m = M.get(j);
-                    n = N.get(j);
-                    sum += m*n;
-                }
+            float result = 0.0f;
+            float m_ij;
+            float n_jk;
+            for (int j = 0; j < col_M; j++) {
+                m_ij = M.containsKey(j) ? M.get(j) : 0.0f;
+                n_jk = N.containsKey(j) ? N.get(j) : 0.0f;
+                result += m_ij * n_jk;
             }
-
-            if (sum != 0.0f){
-                context.write(null, new Text(key.toString() + ","+ Float.toString(sum)));
-            }
-
+            if (result != 0.0f)
+                context.write(null, new Text(key.toString() + "," + result));
         }
     }
 
@@ -95,14 +88,12 @@ public class MatrixMultiplication {
         Configuration conf = new Configuration();
 
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-        if (otherArgs.length != 3){
+        if (otherArgs.length != 2){
             System.err.println("Error");
             System.exit(1);
         }
-
         System.out.println("args[0]: <input0>"+otherArgs[0]);
-        System.out.println("args[1]: <input1>"+otherArgs[1]);
-        System.out.println("args[2]: <output>"+otherArgs[2]);
+        System.out.println("args[1]: <output>"+otherArgs[1]);
 
         Job job = Job.getInstance(conf, "MatrixMultiplication");
         job.getConfiguration().set("columns_N", "3");
@@ -120,8 +111,7 @@ public class MatrixMultiplication {
         job.setOutputValueClass(Text.class);
 
         FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-        FileInputFormat.addInputPath(job, new Path(otherArgs[1]));
-        FileOutputFormat.setOutputPath(job, new Path(otherArgs[2]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
 
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputValueClass(TextOutputFormat.class);
